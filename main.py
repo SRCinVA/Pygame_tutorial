b2abee12e5628e142b028cfb1c09cc08d8547732
SRCinVA <scitarella@hotmail.com>
2021-11-16 21:29:27 -0500

    Created border between halves
    Different ways of drawing rectangles on the field

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("First Game!") # to give a title to the pop-up window
 
 WHITE = (255,255,255)
+BLACK = (0, 0, 0)
+
+BORDER = pygame.Rect(WIDTH/2 - 5, 0, 10, HEIGHT)# to create borders to prevetn ships from colliding
+                                                # we wanted the x to be in the middle of the screen
+                                                # it's -5 because you want it to be half the width of the whole shape
 
 FPS = 60 # defining the refreshing rate at 60 frames per second
 VEL = 5 # a variable of velocity = 5
@@ -15,6 +20,7 @@ SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55,40
 
 def draw_window(red,yellow):
     WIN.fill(WHITE)  # to fill the entire space ...
+    pygame.draw.rect(WIN, BLACK, BORDER)  # drawing a rectangle, but not pygame.Rect()
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))  # we're feeding the values for the rectangles "red" and "yellow" below with coordinates
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update() # we have to manually update the screen after making changes.
@@ -27,7 +33,8 @@ RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets','spaceship_red.png
 RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE,(SPACESHIP_WIDTH, SPACESHIP_HEIGHT)),(270))
 
 def yellow_handle_movement(keys_pressed, yellow):
-    if keys_pressed[pygame.K_a]:             # makes 'a' the left movement
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0:  # makes 'a' the left movement
+                                                        # unclear what role VEL has here
         yellow.x -= VEL # subract from our x value to move
     if keys_pressed[pygame.K_d]:             # makes 'd' the right movement
         yellow.x += VEL # adds to our x value to move
@@ -49,7 +56,7 @@ def red_handle_movement(keys_pressed, red):
 # Creating your main loop
 def main():
     # we'll pass these rectangles over to draw_window()
-    red = pygame.Rect(700,300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)  # x, y, width, height
+    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)  # x, y, width, height
     yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
 
     clock = pygame.time.Clock() # creating a clock object in this loop
@@ -64,7 +71,6 @@ def main():
         keys_pressed = pygame.key.get_pressed() # with every loop it will tell us which keys are being pressed down.
         yellow_handle_movement(keys_pressed, yellow)
         red_handle_movement(keys_pressed, red)
-
         draw_window(red, yellow) # good practice to separate out images from the logic of the game
     
     pygame.quit()
